dc008fb9257f0283857ad7014fd212343be0bdc9
Oluwaseun Lawal <lsoluwaseun@gmail.com>
2022-07-01 14:44:18 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - block to be executed
+ * Description: print all single digits
+ * Return: 0 Success
+ */
+int main(void)
+{
+int c = 0;
+while(c < 10)
+{
+	putchar(48 + c);
+	if(c != 9)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	c++;
+}
+putchar('\n');
+return(0);
+}
