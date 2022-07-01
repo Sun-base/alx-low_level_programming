509042efb6aa65c935d507577517f0258be55533
Oluwaseun Lawal <lsoluwaseun@gmail.com>
2022-07-01 16:12:54 -0700

    test

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,24 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+
 /**
- * main - block to be executed
- * Description: print all single digits
- * Return: 0 Success
- */
+ * main - prints all npossible numbers of single digits
+ * followed by a new line
+ * Return: 0 if (successful)
+*/
+
 int main(void)
 {
-int c = 0;
-while(c < 10)
-{
-	putchar(48 + c);
-	if(c != 9)
-	{
-		putchar(',');
-		putchar(' ');
-	}
-	c++;
-}
-putchar('\n');
-return(0);
+ int n;
+
+ for (n = 48; n < 58; n++)
+ {
+  putchar(n);
+  if (n != 57)
+  {
+   putchar(',');
+   putchar(' ');
+  }
+ }
+ putchar('\n');
+ return (0);
 }
