2bd844ace4f1a2c269d3a6b0f083fe88dc3a297a
Oluwaseun Lawal <lsoluwaseun@gmail.com>
2022-07-01 17:00:12 -0700

    It works

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 
 /**
- * main - prints all npossible numbers of single digits
- * followed by a new line
- * Return: 0 if (successful)
-*/
-
+ *main -> assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
- int n;
+int ch;
+for (ch = 48; ch <= 57; ch++)
+{
 
- for (n = 48; n < 58; n++)
- {
-  putchar(n);
-  if (n != 57)
-  {
-   putchar(',');
-   putchar(' ');
-  }
- }
- putchar('\n');
- return (0);
+putchar(ch);
+if (ch != 57)
+{
+putchar(44);
+putchar(32);
+}
+}
+
+putchar(10); /* this is an ascii code for new line*/
+
+return (0);
 }
